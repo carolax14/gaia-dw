9b248b4d56dfdc83cf232cff66f68103adbce129
carole <carole.hafizou@gmail.com>
2022-02-16 18:01:43 +0400

    Ajout flèche de navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenComponent } from './authen/authen.component';
+import { CompteComponent } from './compte/compte.component';
 
 const routes: Routes = [
-  {
-    path: 'authentification',
-    component: AuthenComponent
-  }
+  { path: 'one', component: AuthenComponent },
+  { path: 'two', component: CompteComponent }
 ];
 
 @NgModule({
